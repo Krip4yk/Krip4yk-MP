c8436a0b5b988a49865f6a7b6c06739f1bf436d4
Krip4yk <krip4yk@gmail.com>
2017-09-14 14:37:53 +0300

    New rep

--- Unit1.cpp
+++ Unit1.cpp
@@ -5,6 +5,8 @@
 
 #include "Unit1.h"
 //---------------------------------------------------------------------------
+#include <fstream.h>
+//---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
@@ -35,7 +37,7 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Edit1Change(TObject *Sender)
 {
-	if (Edit1->Text.Length() != 0) {
+	if (Edit1->Text.Length() != 0 && Edit2->Text.Length() != 0) {
 		Button1->Enabled = true; 
 	} else {
 		Button1->Enabled = false;
@@ -44,10 +46,10 @@ void __fastcall TForm1::Edit1Change(TObject *Sender)
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Edit2Change(TObject *Sender)
 {
-	if (Edit2->Text.Length() != 0) {
-		Button2->Enabled = true; 
+	if (Edit1->Text.Length() != 0 && Edit2->Text.Length() != 0) {
+		Button1->Enabled = true;
 	} else {
-		Button2->Enabled = false;
+		Button1->Enabled = false;
 	}
 }
 //---------------------------------------------------------------------------
@@ -57,15 +59,6 @@ void __fastcall TForm1::Edit3Change(TObject *Sender)
 		Button3->Enabled = true;
 	} else {
 		Button3->Enabled = false;
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::Edit4Change(TObject *Sender)
-{
-	if (Edit4->Text.Length() != 0) {
-		Button4->Enabled = true;
-	} else {
-		Button4->Enabled = false;
 	}
 }
 //---------------------------------------------------------------------------
@@ -81,23 +74,26 @@ void __fastcall TForm1::Button1Clic(TObject *Sender)
 	bool sobaka = false, dot = false;
 	bool text1 = false, text2 = false, text3 = false;
 	//----------------------------------------------
-	int len_text2 = 4;
+	int len_text2 = 5;
 	String *str_text2 = new String[len_text2];
 	str_text2[0] = "gmail";
-	str_text2[1] = "mail";
-	str_text2[2] = "yandex";
-	str_text2[3] = "rambler";
+	str_text2[1] = "hotmail";
+	str_text2[2] = "yahoo";
+	str_text2[3] = "aol";
+	str_text2[4] = "zoho";
 	//----------------------------------------------
-	int len_text3 = 4;
+	int len_text3 = 1;
 	String *str_text3 = new String[len_text3];
-	str_text3[0] = "ru";
-	str_text3[1] = "ua";
-	str_text3[2] = "com";
-	str_text3[3] = "hu";
+	str_text3[0] = "com";
+	//str_text3[1] = "org";
+	//str_text3[2] = "net";
+	//----------------------------------------------
+	int indsob = 0, inddot = 0;
 	//----------------------------------------------
 	for (int i = 1; i <= Edit1->Text.Length(); i++) {
 		if (Edit1->Text[i] == '@') {
 			sobaka = true;
+			indsob = i;
 			if (i == 1) {
 				break;
 			} else {
@@ -106,26 +102,44 @@ void __fastcall TForm1::Button1Clic(TObject *Sender)
 		}
 		if (Edit1->Text[i] == '.') {
 			dot = true;
+			inddot = i;
 			if (i == 0) {
                 break;
 			} else {
 				if (Edit1->Text[i-1] == '@') {
 					break;
 				} else {
-                	text2 = true;
+					text2 = true;
+					for (int j = 0; j < len_text2; j++) {
+						if (Edit1->Text.SubString(indsob+1, inddot-indsob-1) == str_text2[j]) {
+							break;
+						}
+						if (j == len_text2-1) {
+							text2 = false;
+						}
+					}
 				}
 			}
 			if (i == Edit1->Text.Length()) {
 				break;
 			} else {
 				text3 = true;
+				for (int j = 0; j < len_text3; j++) {
+					if (Edit1->Text.SubString(inddot+1, Edit1->Text.Length()-1) == str_text3[j]) {
+                        break;
+					} else {
+                        text3 = false;
+                    }
+				}
 				break;
 			}
 		}
 	}
 	if (sobaka && dot && text1 && text2 && text3) {    
 		Memo1->Lines->Add(Edit1->Text);
-		Edit1->Text = "";              
+		Edit1->Text = "";
+		Memo2->Lines->Add(Edit2->Text);
+		Edit2->Text = "";
 	} else {
 		Memo3->Lines->Add("Error 0. Incorrect mail");
 		if (!sobaka) {
@@ -138,27 +152,27 @@ void __fastcall TForm1::Button1Clic(TObject *Sender)
 			Memo3->Lines->Add("	no text before '@'");
 		}
 		if (!text2) {
-			Memo3->Lines->Add("	no text after '@'");
+			Memo3->Lines->Add("	no text after '@' or incorrect mail");
+			Memo3->Lines->Add("		correct mails:");
+			for (int i = 0; i < len_text2; i++) {
+				Memo3->Lines->Add("			" + str_text2[i]);
+			}
 		}
 		if (!text3) {
-			Memo3->Lines->Add("	no text after '.'");
+			Memo3->Lines->Add("	no text after '.' or incorrect domain");
+			Memo3->Lines->Add("		correct domais:");
+			for (int i = 0; i < len_text3; i++) {
+				Memo3->Lines->Add("			" + str_text3[i]);
+			}
 		}
 	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::Button2Click(TObject *Sender)
-{
-	Memo2->Lines->Add(Edit2->Text);
-	Edit2->Text = "";	
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Button5Click(TObject *Sender)
 {
 	if (Memo1->Lines->Count != 0 && Memo2->Lines->Count != 0) {
 		for (int i = 0; i < Memo1->Lines->Count; i++) {
-			for (int j = 0; j < Memo2->Lines->Count; j++) {
-				Memo3->Lines->Add("Trying mail: " + Memo1->Lines->Strings[i] + " password: " + Memo2->Lines->Strings[j]);
-			}
+		   	Memo3->Lines->Add("Trying mail: " + Memo1->Lines->Strings[i] + " password: " + Memo2->Lines->Strings[i]);
 		}
 	} else {
 		if (Memo1->Lines->Count == 0) {
@@ -187,59 +201,89 @@ void __fastcall TForm1::Edit5Change(TObject *Sender)
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TForm1::Button6Click(TObject *Sender)
-{
-	Button7->Enabled = true;
-	Button6->Enabled = false;
-	Button9->Enabled = true;
-	Button10->Enabled = true;
-	Edit5->Enabled = true;
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TForm1::Button7Click(TObject *Sender)
-{
-	Button7->Enabled = false;
-	Button6->Enabled = true;
-	Button9->Enabled = true;
-	Button10->Enabled = true;
-	Edit5->Enabled = true;
-}
-//---------------------------------------------------------------------------
-
 void __fastcall TForm1::Button8Click(TObject *Sender)
 {
-	//
+	if (Edit5->Text.ToInt() > 0) {
+		if (Memo1->Lines->Count != 0) {
+			Memo1->Lines->Delete(Edit5->Text.ToInt()-1);
+		} else {
+			Memo3->Lines->Add("Error 2. Mail list is clear.");
+		}
+		if (Memo2->Lines->Count != 0) {
+			Memo2->Lines->Delete(Edit5->Text.ToInt()-1);
+		} else {
+			Memo3->Lines->Add("Error 2. Password list is clear.");
+		}
+	} else {
+        Memo3->Lines->Add("Error 3. Index incorrect. Index must be > 0.");
+    }
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::Button9Click(TObject *Sender)
 {
-	if (!Button6->Enabled) {
-		if (Memo1->Lines->Count != 0) {
-	        Memo1->Lines->Delete(Memo1->Lines->Count);
-		} else {
-			Memo3->Lines->Add("Error 2. Mail list is clear.");
-        }
+	if (Memo1->Lines->Count != 0) {
+		Memo1->Lines->Delete(Memo1->Lines->Count-1);
 	} else {
-		if (Memo2->Lines->Count != 0) {
-	        Memo2->Lines->Delete(Memo1->Lines->Count);
-		} else {
-			Memo3->Lines->Add("Error 2. Password list is clear.");
-        }
-    }
+		Memo3->Lines->Add("Error 2. Mail list is clear.");
+	}
+	if (Memo2->Lines->Count != 0) {
+		Memo2->Lines->Delete(Memo1->Lines->Count-1);
+	} else {
+		Memo3->Lines->Add("Error 2. Password list is clear.");
+	}
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::Button10Click(TObject *Sender)
 {
-	if (!Button6->Enabled) {
-		Memo1->Lines->Clear();
-		Memo3->Lines->Add("Mail list is clear.");
+	Memo1->Lines->Clear();
+	Memo3->Lines->Add("Mail list is clear.");
+	Memo2->Lines->Clear();
+	Memo3->Lines->Add("Password list is clear.");
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button3Click(TObject *Sender)
+{
+	ifstream fi(Edit3->Text.c_str());
+	if (fi.is_open()) {
+		fi.close();
+		Memo5->Lines->LoadFromFile(Edit3->Text);
+		for (int i = 0; i < Memo5->Lines->Count; i++) {
+			bool fatr = false;
+			String mail = "", password = "";
+			for (int j = 1; j <= Memo5->Lines->Strings[i].Length(); j++) {
+				if (Memo5->Lines->Strings[i][j] == ':') {
+					fatr = true;
+					continue;
+				}
+				if (!fatr) {
+                	mail += Memo5->Lines->Strings[i][j];    
+				} else {
+                	password += Memo5->Lines->Strings[i][j];
+                }
+			}
+			Memo1->Lines->Add(mail);
+			Memo2->Lines->Add(password);
+		}
 	} else {
-		Memo2->Lines->Clear();
-		Memo3->Lines->Add("Password list is clear.");
-    }
+        Memo3->Lines->Add("Error 4. Can't open file. Please check file or path file");
+		fi.close();
+	}
+	Memo5->Lines->Clear();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Memo3Change(TObject *Sender)
+{
+	Memo3->Lines->SaveToFile("Log.txt");
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Memo4Change(TObject *Sender)
+{
+	Memo4->Lines->SaveToFile("Valid.txt");
 }
 //---------------------------------------------------------------------------
 
--- Unit1.dfm
+++ Unit1.dfm
@@ -4,7 +4,7 @@ object Form1: TForm1
   BorderStyle = bsSingle
   Caption = 'Project MP by Krip4yk'
   ClientHeight = 588
-  ClientWidth = 837
+  ClientWidth = 650
   Color = clNavy
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -15,32 +15,6 @@ object Form1: TForm1
   Position = poDesktopCenter
   PixelsPerInch = 96
   TextHeight = 13
-  object Label1: TLabel
-    Left = 8
-    Top = 173
-    Width = 45
-    Height = 13
-    Caption = 'From file:'
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWhite
-    Font.Height = -11
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
-  end
-  object Label2: TLabel
-    Left = 88
-    Top = 173
-    Width = 26
-    Height = 13
-    Caption = 'Path:'
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWhite
-    Font.Height = -11
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
-  end
   object Memo1: TMemo
     Left = 267
     Top = 8
@@ -82,7 +56,7 @@ object Form1: TForm1
   object Memo3: TMemo
     Left = 8
     Top = 423
-    Width = 821
+    Width = 635
     Height = 157
     Hint = 'Log'
     Color = clBlack
@@ -97,6 +71,7 @@ object Form1: TForm1
     ScrollBars = ssBoth
     ShowHint = True
     TabOrder = 2
+    OnChange = Memo3Change
   end
   object Edit1: TEdit
     Left = 40
@@ -118,7 +93,7 @@ object Form1: TForm1
   end
   object Edit2: TEdit
     Left = 40
-    Top = 79
+    Top = 35
     Width = 185
     Height = 21
     Color = clHotLight
@@ -134,43 +109,25 @@ object Form1: TForm1
     OnChange = Edit2Change
   end
   object Button1: TButton
-    Left = 88
-    Top = 35
+    Left = 89
+    Top = 62
     Width = 75
     Height = 38
     Cursor = crHandPoint
-    Caption = 'Add mail'
+    Caption = 'Add'
     Enabled = False
     TabOrder = 5
     OnClick = Button1Clic
   end
-  object Button2: TButton
-    Left = 88
-    Top = 106
-    Width = 75
-    Height = 38
-    Caption = 'Add password'
-    Enabled = False
-    TabOrder = 6
-    OnClick = Button2Click
-  end
   object Button3: TButton
     Left = 8
-    Top = 192
+    Top = 112
     Width = 75
     Height = 25
-    Caption = 'Add mail'
+    Caption = 'Add from file:'
     Enabled = False
-    TabOrder = 7
-  end
-  object Button4: TButton
-    Left = 8
-    Top = 223
-    Width = 75
-    Height = 25
-    Caption = 'Ass password'
-    Enabled = False
-    TabOrder = 8
+    TabOrder = 6
+    OnClick = Button3Click
   end
   object Memo4: TMemo
     Left = 8
@@ -189,16 +146,43 @@ object Form1: TForm1
     ReadOnly = True
     ScrollBars = ssBoth
     ShowHint = True
-    TabOrder = 9
+    TabOrder = 7
+    OnChange = Memo4Change
   end
   object Edit3: TEdit
     Left = 89
-    Top = 196
+    Top = 116
     Width = 172
     Height = 21
     Hint = 'sometext@somemail.domen'
     Color = clHotLight
-    Enabled = False
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWhite
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 8
+    OnChange = Edit3Change
+  end
+  object Button5: TButton
+    Left = 160
+    Top = 174
+    Width = 89
+    Height = 83
+    Caption = 'Start'
+    TabOrder = 9
+    OnClick = Button5Click
+  end
+  object Edit5: TEdit
+    Left = 89
+    Top = 174
+    Width = 41
+    Height = 21
+    Hint = 'sometext@somemail.domen'
+    Color = clHotLight
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWhite
     Font.Height = -11
@@ -208,129 +192,42 @@ object Form1: TForm1
     ParentShowHint = False
     ShowHint = True
     TabOrder = 10
-    OnChange = Edit3Change
-  end
-  object Edit4: TEdit
-    Left = 89
-    Top = 227
-    Width = 172
-    Height = 21
-    Hint = 'sometext@somemail.domen'
-    Color = clHotLight
-    Enabled = False
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWhite
-    Font.Height = -11
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
-    ParentShowHint = False
-    ShowHint = True
-    TabOrder = 11
-    OnChange = Edit4Change
-  end
-  object Button5: TButton
-    Left = 664
-    Top = 351
-    Width = 145
-    Height = 58
-    Caption = 'Start'
-    TabOrder = 12
-    OnClick = Button5Click
-  end
-  object Edit5: TEdit
-    Left = 760
-    Top = 54
-    Width = 41
-    Height = 21
-    Hint = 'sometext@somemail.domen'
-    Color = clHotLight
-    Enabled = False
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWhite
-    Font.Height = -11
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
-    ParentShowHint = False
-    ShowHint = True
-    TabOrder = 13
     OnChange = Edit5Change
   end
   object Button8: TButton
-    Left = 664
-    Top = 52
+    Left = 8
+    Top = 172
     Width = 75
     Height = 25
     Caption = 'Delete at :'
     Enabled = False
-    TabOrder = 14
+    TabOrder = 11
     OnClick = Button8Click
   end
-  object Button6: TButton
-    Left = 664
-    Top = 21
-    Width = 75
-    Height = 25
-    Caption = 'Mail'
-    TabOrder = 15
-    OnClick = Button6Click
-  end
-  object Button7: TButton
-    Left = 745
-    Top = 21
-    Width = 75
-    Height = 25
-    Caption = 'Password'
-    TabOrder = 16
-    OnClick = Button7Click
-  end
   object Button9: TButton
-    Left = 664
-    Top = 83
+    Left = 8
+    Top = 203
     Width = 75
     Height = 25
     Caption = 'Delete last'
-    Enabled = False
-    TabOrder = 17
+    TabOrder = 12
     OnClick = Button9Click
   end
-  object Panel1: TPanel
-    Left = 649
-    Top = 147
-    Width = 180
-    Height = 113
-    Caption = 'Working...'
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWhite
-    Font.Height = -11
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
-    TabOrder = 18
-  end
-  object Panel2: TPanel
-    Left = 8
-    Top = 192
-    Width = 253
-    Height = 72
-    Caption = 'Working...'
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWhite
-    Font.Height = -11
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
-    TabOrder = 19
-  end
   object Button10: TButton
-    Left = 664
-    Top = 112
+    Left = 8
+    Top = 232
     Width = 75
     Height = 25
     Caption = 'Clear all'
-    Enabled = False
-    TabOrder = 20
+    TabOrder = 13
     OnClick = Button10Click
+  end
+  object Memo5: TMemo
+    Left = 649
+    Top = 35
+    Width = 185
+    Height = 126
+    TabOrder = 14
+    Visible = False
   end
 end
